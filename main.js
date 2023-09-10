ef079d5515cc311db835cdb322b02ae4b24d20fa
Ritvik Ray <69351614+RitvikRay@users.noreply.github.com>
2023-09-10 15:49:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+ function new_image()
+ {
+     fabric.Image.fromURL('BirthdayImage.jpg', function(Img)
+     {
+         block_image__object = Img;
+         block_image_object.scaletoWidth(block_image_width);
+         block_image_object.scaletoHeight(block_image_width);
+         block_image_object.set({
+             top:player_y,
+             left:player_x
+         });
+         canvas.add(block_image_object);
+     });
+ }
+
+function playSound(){
+    x.play;
+}
